27f108f109f5c37b379c4413146cb61a720eb1b4
EddyKaggia <kagzcodes@gmail.com>
2024-03-27 12:37:00 -0700

    feat: complete in todo temp solution

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 import { TodoItem } from "./todoItem.js";
 import { TodoCollection } from "./todoCollection.js";
+//inquirer library to create a command-line interface for interacting with the todo list application
 import inquirer from "inquirer";
 let todos = [
     new TodoItem(1, "Buy Flowers"),
@@ -8,15 +9,58 @@ let todos = [
     new TodoItem(4, "Call Joe", true),
 ];
 let collection = new TodoCollection("Adam", todos);
+let showCompleted = true;
 function displayTodoList() {
     console.log(`${collection.userName}'s Todo List ` +
         `(${collection.getItemCounts().incomplete} items to do)`);
-    collection.getTodoItems(true).forEach((item) => item.printDetails());
+    collection.getTodoItems(showCompleted).forEach((item) => item.printDetails());
 }
+// "Enums" is short for "Enumerations." Enumerations in programming are a way to define a type consisting of a set of named constants, often referred to as enumerators or members. These constants represent a finite set of possible values that a variable of that type can have
+// An enum Commands is declared with two members: Toggle and Quit. Each member is assigned a string value representing a command option for the todo list application.
 var Commands;
 (function (Commands) {
+    Commands["Add"] = "Add New Task";
+    Commands["Complete"] = "Complete Task";
+    Commands["Toggle"] = "Show/Hide Completed";
+    Commands["Purge"] = "Remove Completed Tasks";
     Commands["Quit"] = "Quit";
 })(Commands || (Commands = {}));
+function promptAdd() {
+    console.clear();
+    inquirer
+        .prompt({
+        type: "input",
+        name: "add",
+        message: "Enter task: ",
+    })
+        .then((answers) => {
+        if (answers["add"] !== "") {
+            collection.addTodo(answers["add"]);
+        }
+        promptUser();
+    });
+}
+function promptComplete() {
+    console.clear();
+    inquirer
+        .prompt({
+        type: "checkbox",
+        name: "complete",
+        message: "Mark Tasks Complete",
+        choices: collection.getTodoItems(showCompleted).map((item) => ({
+            name: item.task,
+            value: item.id,
+            checked: item.complete,
+        })),
+    })
+        .then((answers) => {
+        let completedTasks = answers["complete"];
+        collection
+            .getTodoItems(true)
+            .forEach((item) => collection.markComplete(item.id, completedTasks.find((id) => id === item.id) != undefined));
+        promptUser();
+    });
+}
 function promptUser() {
     console.clear();
     displayTodoList();
@@ -26,10 +70,29 @@ function promptUser() {
         name: "command",
         message: "Choose option",
         choices: Object.values(Commands),
+        //badProperty: true,
     })
         .then((answers) => {
-        if (answers["command"] !== Commands.Quit) {
-            promptUser();
+        switch (answers["command"]) {
+            case Commands.Toggle:
+                showCompleted = !showCompleted;
+                promptUser();
+                break;
+            case Commands.Add:
+                promptAdd();
+                break;
+            case Commands.Complete:
+                if (collection.getItemCounts().incomplete > 0) {
+                    promptComplete();
+                }
+                else {
+                    promptUser();
+                }
+                break;
+            case Commands.Purge:
+                collection.removeComplete();
+                promptUser();
+                break;
         }
     });
 }
